9f2ef9c9a071a7674acecaa13b1599f037a6f57f
andai06 <andy.tuzeyi@gmail.com>
2025-07-20 00:17:53 +0400

    add bycript

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import path from 'path';
+
 import project from './routes/project';
 import task from './routes/task';
+import {comparePassword, hashPassword} from './utils/password';
 
 const app = express();
 const PORT: number = 3000;
+
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
 
 app.set('view engine', 'pug');
 console.log(__dirname);
---
+++ utils/password.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt';
+
+/**
+ * Hash a given password.
+ * @param {string} password - Password to hash.
+ * @returns {Promise<string>} Password's hash.
+ */
+export async function hashPassword(pass: string) {
+    return await bcrypt.hash(pass, 10);
+}
+
+export async function comparePassword(pass: string, hash: string) {
+  return await bcrypt.compare(pass, hash);
+}
